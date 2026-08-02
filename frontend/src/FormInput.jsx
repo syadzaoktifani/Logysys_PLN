ffe633bf0a1aa35fd963f520ee25fee6bc1a3419
Syadza Oktifani <155104200+syadzaoktifani@users.noreply.github.com>
2026-08-02 21:56:16 +0700

    Update FormInput.jsx

--- frontend/src/FormInput.jsx
+++ frontend/src/FormInput.jsx
@@ -111,7 +111,7 @@ export default function FormInput() {
       targetSheet: 'MaterialKeluarUnit' // PENTING: Identitas agar masuk ke sheet yang benar
     };
 
-    const res = await fetch('https://logysys-pln.vercel.app/api/login/api/input-manual', {
+    const res = await fetch('https://logysys-pln.vercel.app/api/input-manual', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload)
@@ -245,8 +245,8 @@ export default function FormInput() {
     : 'text-slate-500 hover:text-slate-700'
   }`}
 >
-  <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
-  Dashboard
+  
+ 
 </button>
             </div>
           </div>
