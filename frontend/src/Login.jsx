c8dfb1d7c02f6232412b7d329cfce94bcb3d9193
Syadza Oktifani <155104200+syadzaoktifani@users.noreply.github.com>
2026-08-02 21:02:56 +0700

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
     setErrorMsg('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch('https://logysys-pln.vercel.app/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
