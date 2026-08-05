c984c3079ec02e48a49cedd1c033a0fd4a9428ee
Syadza Oktifani <155104200+syadzaoktifani@users.noreply.github.com>
2026-08-05 10:26:07 +0700

    Update FormInput.jsx

--- frontend/src/FormInput.jsx
+++ frontend/src/FormInput.jsx
@@ -435,7 +435,7 @@ export default function FormInput() {
                     label="Lokasi Pemasangan" name="lokasi" value={manualForm.lokasi} onChange={handleManualChange} disabled={isLoading} placeholder="Alamat atau nama gardu..." className="md:col-span-12 lg:col-span-6" 
                     icon={<svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>}
                   />
-                  <InputField label="Keterangan (Opsional)" name="keterangan" value={manualForm.keterangan} onChange={handleManualChange} disabled={isLoading} placeholder="Catatan tambahan bila ada..." className="md:col-span-12 lg:col-span-6" />
+                  <InputField label="Keterangan" name="keterangan" value={manualForm.keterangan} onChange={handleManualChange} disabled={isLoading} placeholder="Catatan tambahan bila ada..." className="md:col-span-12 lg:col-span-6" />
 
                 </div>
 
